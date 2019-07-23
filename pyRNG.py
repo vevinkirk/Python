47278e108a48ab6b322ffe38c4edca550a5d9f11
KevinVi <kevin@debian.com>
2019-07-23 11:18:36 -0700

    adding dictionary support

--- pyRNG.py
+++ pyRNG.py
@@ -1,9 +1,11 @@
 import random
 
 def main():
-    lst = userInput()
-    test = randomInput(lst)
-    print(test)
+    #lst = userInput()
+    #test = randomInput(lst)
+   # print(test)
+    print(dictInput())
+
 def userInput():
     userInput  = [int(x) for x in input().split()]
     return userInput
@@ -12,6 +14,17 @@ def randomInput(lst):
     maxrange = max(lst)
     minrange = min(lst)
     length = len(lst)
-    randlst = random.sample(range(maxrange), length)
+    randlst = random.sample(lst, length)
     return randlst
+
+def dictInput():
+    class_list = dict()
+    data = input('Enter name & score separated by ":" ')
+    temp = data.split(':')
+    class_list[temp[0]] = int(temp[1])
+    print(class_list.items()) 
+    # Displaying the dictionary
+    for key, value in class_list.items():
+    	print('Name: {}, Score: {}'.format(key, value))
+    
 main()
